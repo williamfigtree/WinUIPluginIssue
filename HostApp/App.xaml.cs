b0f1e712440c7ac8f9fecee6307a5e882629e07b
William Figtree <William.Figtree@resmed.com.au>
2021-11-12 18:01:45 +1100

    Add project files.

---
+++ HostApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.IO;
+
+namespace HostApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            // Attempt to load a UserControl from a plugin dll
+            // PluginUserControl throws an XamlParsingExcpetion during instantiation
+
+            // Locate plugin dll in the Plugin project bin directory
+            var rootPath = Path.GetFullPath(@"..\..\..\..\..\..\..\..\", typeof(Program).Assembly.Location);
+            var pluginDllPath = Path.Combine(rootPath, @"Plugin\bin\x86\Debug\net5.0-windows10.0.19041.0\Plugin.dll");
+
+            // Instantiate PluginUserControl
+            var pluginLoadContext = new PluginLoadContext(pluginDllPath);
+            using (pluginLoadContext.EnterContextualReflection())
+            {
+                var pluginUserControl = Activator.CreateInstance("Plugin", "Plugin.PluginUserControl");
+            }
+        }
+    }
+}
---
+++ Plugin/PluginUserControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Plugin
+{
+    public sealed partial class PluginUserControl : UserControl
+    {
+        public PluginUserControl()
+        {
+            // This line throws XamlParsingException - Cannot locate resource from 'ms-appx:///Plugin/PluginUserControl.xaml'
+            this.InitializeComponent();
+        }
+    }
+}
